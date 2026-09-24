39bb4d481b49b0099218bb68e684d8eabfb8993b
vikash-agribridge <vikash@agribridge.in>
2026-09-24 09:34:46 +0530

    cleaned

--- src/Screens/ReactNativeReanimated/LoaderAnimation.js
+++ src/Screens/ReactNativeReanimated/LoaderAnimation.js
@@ -6,13 +6,31 @@ import {
 
 import Loader from "./Loader";
 import ChangingShapeLoader from "./ChangingShapeLoader";
+import AnimatedBorderBox from "./AnimatedBorderBox";
 
 export default function LoaderAnimation() {
 
     return (
         <View style={styles.container}>
-            <Loader />
-            <ChangingShapeLoader />
+            {/* <Loader />
+            <ChangingShapeLoader /> */}
+
+            <View style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}>
+                <AnimatedBorderBox
+                    width={300}
+                    height={200}
+                    borderWidth={4}
+                    borderRadius={24}
+                    segmentLength={100}
+                    duration={2200}
+                    borderColor="#D8DCE1"
+                    glowColor="#0099FF"
+                />
+            </View>
         </View>
     );
 }
